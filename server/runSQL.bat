625c00f8fce89f0be04011d8bd76723af6bf277a
Ollie Roozen <mrbondusa5@gmail.com>
2018-06-08 09:04:23 +1200

    Removed old SQL code

--- server/runSQL.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-mode con: cols=180 lines=30
-color 0A
-"C:\Program Files\MySQL\mysql-5.7.18-winx64\bin\mysqld" --console
-PAUSE
--- server/shutdownSQL.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-mode con: cols=180 lines=30
-color 0C
-"C:\Program Files\MySQL\mysql-5.7.18-winx64\bin\mysqladmin" -u root shutdown
-PAUSE
